0b615c901066da60369a01ab77914f5f682ed49d
anderdl <anderdl007@gmail.com>
2015-05-12 01:29:23 -0500

    second commit

--- photologue/views.py
+++ photologue/views.py
@@ -1,5 +1,5 @@
 import warnings
-
+from django.conf import settings
 from django.views.generic.dates import ArchiveIndexView, DateDetailView, DayArchiveView, MonthArchiveView, \
     YearArchiveView
 from django.views.generic.detail import DetailView
